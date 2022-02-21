c434095fb2a578cb8b96b26eca5d702be1136c37
w0ngja <jasonw2000@hotmail.com>
2022-02-21 23:34:49 +1100

    Update 1.Two_Sum.py

--- python/1.Two_Sum.py
+++ python/1.Two_Sum.py
@@ -1,4 +1,5 @@
-class Solution:    def twoSum(self, nums: List[int], target: int) -> List[int]:
+class Solution:    
+        def twoSum(self, nums: List[int], target: int) -> List[int]:
         values = {}
         for idx, value in enumerate(nums):
             if target - value in values:
